46f63843c211292249ff20747e8ecf61f327661f
Sergio Henrique <sergiohenriquetp@gmail.com>
2016-06-06 12:42:22 -0300

    refactoring string messages of painel options

--- src/projetoFinal/PainelDeOpcoes.java
+++ src/projetoFinal/PainelDeOpcoes.java
@@ -8,14 +8,17 @@ import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
 public class PainelDeOpcoes extends JPanel implements ActionListener{
-static JPanel  painelDeOpcoes;
+
+	static JPanel  painelDeOpcoes;
+	static private String startText = "Ok, let's start !";
+	static private String quitText = "No, please quit the game ?";
 	
 	public PainelDeOpcoes() {
 		painelDeOpcoes = new JPanel();
-		JButton buttonStart = new JButton("Ok, let's start !");
+		JButton buttonStart = new JButton(startText);
 		buttonStart.setSize(20, 30);
 		buttonStart.addActionListener(this);
-		JButton buttonQuit = new JButton("No, please quit the game ?");
+		JButton buttonQuit = new JButton(quitText);
 		buttonQuit.setSize(20, 30);
 		buttonQuit.addActionListener(this);
 		this.add(buttonStart);
@@ -26,12 +29,12 @@ static JPanel  painelDeOpcoes;
 	public void actionPerformed(ActionEvent e) {
 		String comando = e.getActionCommand();
 		
-		if (comando == "Ok, let's start !"){
+		if (comando == startText){
 			Frame frame = new Frame();
 			frame.destruirPainel();
 			frame.criarPainelDeConteudo("");
 		}
-		else if (comando == "No, please quit the game ?"){
+		else if (comando == quitText){
 			System.exit(0);
 		}
 		
